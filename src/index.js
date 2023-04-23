24aedd1c605b6c2960eccb21eea3e1da193c7890
G. Nomika <nomigajula412@gmail.com>
2023-04-23 23:17:13 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -289,7 +289,7 @@ res.send("wrong details");
     res.render("home_1")
     })
 let port =process.env.PORT;
-if(port==null ||port=="){
+if(port==null ||port==""){
    port=3000;
    }
 app.listen(port,()=>{
